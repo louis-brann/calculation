611ab7590e21c69eb4f855c645b9cee30ae6afd9
Louie Brann <louis.brann@gmail.com>
2018-08-13 08:44:02 -0700

    actually working (random_deck + can_play_on_foundation fixes). printing cleanup. RandomPlayer and GreedyPlayer

--- calculation-refactor.py
+++ calculation-refactor.py
@@ -24,18 +24,36 @@ class CalculationBoard:
     The calculation board is a snapshot of where all the cards are.
     """
 
-    NUM_FOUNDATIONS = 4
-    NUM_WASTES = 4
-    NUM_PILES = NUM_FOUNDATIONS + NUM_WASTES
+    NUM_WASTES = NUM_FOUNDATIONS = NUM_SUITS = 4
 
     class PileTypes:
-        FOUNDATION = 1
-        WASTE = 2
-        DECK = 3
+        FOUNDATION = "F"
+        WASTE = "W"
+        DECK = "D"
+
+    class CardLocation:
+        def __init__(self, pile_type=None, pile_index=None):
+            self.pile_type = pile_type
+            self.pile_index = pile_index
+
+        def __repr__(self):
+            return "{type}{index}".format(type=self.pile_type, index=self.pile_index)
+
+    class Move:
+        def __init__(self, src=None, dest=None):
+            self.src = src
+            self.dest = dest
+
+        def __repr__(self):
+            return "({src} -> {dest})".format(src=self.src, dest=self.dest)
+
+    class InvalidMoveException(Exception):
+        pass
 
     # TODO: do we need to pass in cards_per_suit and deck?
     # they were relics of tree-searching, but that may not be necessary.
     def __init__(self, cards_per_suit=13, deck=None):
+        self.cards_per_suit = cards_per_suit
         self.foundations = [[i] for i in range(1, 1+CalculationBoard.NUM_FOUNDATIONS)]
         self.wastes = [[] for i in range(CalculationBoard.NUM_WASTES)]
 
@@ -52,11 +70,13 @@ class CalculationBoard:
 
     @staticmethod
     def generate_random_deck(cards_per_suit):
-        values = list(range(1, cards_per_suit)) + [0]
-        all_values = (values * 4)
-        non_foundation = all_values[4:]
-        random.shuffle(non_foundation)
-        return all_values[:4] + non_foundation
+        suit = list(range(1, cards_per_suit)) + [0]
+        # unshuffled deck (suits in order)
+        deck =  suit * CalculationBoard.NUM_SUITS
+        # foundation cards start out in foundation piles, not deck
+        deck = deck[CalculationBoard.NUM_FOUNDATIONS:]
+        random.shuffle(deck)
+        return deck
 
     def is_winning(self):
         return self.foundations == self.winning
@@ -65,6 +85,18 @@ class CalculationBoard:
         moves_from_waste_piles = self.get_possible_moves_from_waste()
         moves_from_deck = self.get_possible_moves_from_deck()
         return moves_from_waste_piles + moves_from_deck
+
+    def can_play_on_foundation(self, card_to_play, foundation_index):
+        foundation = self.foundations[foundation_index]
+
+        # If we've already reached king, that pile's already done. Can't play any more
+        top_card = foundation[-1]
+        if top_card == 0:
+            return False
+
+        base_card = foundation[0]
+        expected_next = (top_card + base_card) % self.cards_per_suit
+        return card_to_play == expected_next
 
     def get_possible_moves_from_waste(self):
         """
@@ -77,15 +109,13 @@ class CalculationBoard:
             if not waste:
                 continue
 
-            src = CardLocation(pile_type=CalculationBoard.PileTypes.WASTE, pile_index=i)
-            top_card = waste[-1]
+            src = CalculationBoard.CardLocation(pile_type=CalculationBoard.PileTypes.WASTE, pile_index=i)
+            src_card = waste[-1]
             for j in range(len(self.foundations)):
-                foundation = self.foundations[j]
-                increment = base_card = foundation[0]
-                next_expected = foundation[-1] + increment
-                if top_card == next_expected:
-                    dest = CardLocation(pile_type=CalculationBoard.PileTypes.FOUNDATION, pile_index=j)
-                    possible_moves.append(Move(src=src, dest=dest))
+                dest = CalculationBoard.CardLocation(pile_type=CalculationBoard.PileTypes.FOUNDATION, pile_index=j)
+                if self.can_play_on_foundation(src_card, j):
+                    possible_moves.append(CalculationBoard.Move(src=src, dest=dest))
+
         return possible_moves
 
     def get_possible_moves_from_deck(self):
@@ -93,22 +123,19 @@ class CalculationBoard:
         if not self.deck:
             return possible_moves
 
-        top_card = self.deck[-1]
-        src = CardLocation(pile_type=CalculationBoard.PileTypes.DECK, pile_index=0)
+        src_card = self.deck[-1]
+        src = CalculationBoard.CardLocation(pile_type=CalculationBoard.PileTypes.DECK, pile_index=0)
 
         # deck to foundations
         for j in range(len(self.foundations)):
-            foundation = self.foundations[j]
-            increment = base_card = foundation[0]
-            next_expected = foundation[-1] + increment
-            if top_card == next_expected:
-                dest = CardLocation(pile_type=CalculationBoard.PileTypes.FOUNDATION, pile_index=j)
-                possible_moves.append(Move(src=src, dest=dest))
+            dest = CalculationBoard.CardLocation(pile_type=CalculationBoard.PileTypes.FOUNDATION, pile_index=j)
+            if self.can_play_on_foundation(src_card, j):
+                possible_moves.append(CalculationBoard.Move(src=src, dest=dest))
 
         # deck to waste --> all waste are playable
         for i in range(len(self.wastes)):
-            dest = CardLocation(pile_type=CalculationBoard.PileTypes.WASTE, pile_index=i)
-            possible_moves.append(Move(src=src, dest=dest))
+            dest = CalculationBoard.CardLocation(pile_type=CalculationBoard.PileTypes.WASTE, pile_index=i)
+            possible_moves.append(CalculationBoard.Move(src=src, dest=dest))
 
         return possible_moves
 
@@ -134,45 +161,67 @@ class CalculationBoard:
 
         return new_board
 
-CardLocation = namedtuple('CardLocation', ['pile_type', 'pile_index'])
-Move = namedtuple('Move', ['src', 'dest'])
-class InvalidMoveException(Exception):
-    pass
-
 class CalculationPlayer:
     def choose_best_move(self, possible_moves):
         """
         core logic for the player. subclasses should overwrite to implement
         their own logic
         """
+        pass
+
+class RandomPlayer(CalculationPlayer):
+    def choose_best_move(self, possible_moves):
         return random.choice(possible_moves)
+
+class GreedyPlayer(CalculationPlayer):
+    def choose_best_move(self, possible_moves):
+        # list of (weight, move)
+        weighted_moves = [(self.get_move_weight(move), move) for move in possible_moves]
+        weighted_moves.sort()
+        return weighted_moves[0][1]
+
+    def get_move_weight(self, move):
+        if move.dest.pile_type == CalculationBoard.PileTypes.FOUNDATION:
+            return 1
+        else:
+            return 2
+
+def play_game(board, player):
+    """
+    Automates playthrough from board state with given player's playing strategy
+    """
+    print "===== STARTING ====="
+    while not board.is_winning():
+        print str(board)
+        possible_moves = board.get_possible_moves()
+        if not possible_moves:
+            print "===== LOST :( ====="
+            return False
+
+        move = player.choose_best_move(possible_moves)
+        new_board = CalculationBoard.apply_move_to_board(board, move)
+        board = new_board
+
+    print str(board)
+    print "===== WON!!! :D ====="
+    return True
+    # TODO: keep track of moves
 
 # Main Function
 
 def main():
     cards_per_suit = 5
     board = CalculationBoard(cards_per_suit)
-    player = CalculationPlayer()
-    while not board.is_winning():
-        possible_moves = board.get_possible_moves()
-        if not possible_moves:
-            print "===== NO MORE MOVES ====="
-            print "Last board: " + str(board)
+    player = RandomPlayer()
+
+    num_attempts = 0
+    max_attempts_per_game = 5
+    while num_attempts < max_attempts_per_game:
+        result = play_game(board, player)
+        if result:
+            print "YAYYYY"
             return
-
-        move = player.choose_best_move(possible_moves)
-        # try:
-        new_board = CalculationBoard.apply_move_to_board(board, move)
-        # except InvalidMoveException:
-        #     print "=== ERROR ==="
-        #     print board
-        #     for move in possible_moves:
-        #         print "src: {src}, dest: {dest}".format(src=move.src, dest=move.dest)
-        #     return
-        board = new_board
-
-    print "===== WON!!! ====="
-    # TODO: keep track of moves
+        num_attempts += 1
 
 if __name__ == "__main__":
     main()
